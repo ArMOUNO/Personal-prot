a7442def6c3390a7970b56c769054762a7601817
ArMouno <armouno8@gmail.com>
2024-05-07 18:05:10 +0600

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["*"],
   theme: {
     extend: {},
     fontFamily:{
-      hero:["Sedan SC","serif"],
+      'hero':["Sedan SC", 'serif'],
+      'mono': ['ui-monospace', 'SFMono-Regular',],
     }
   },
   plugins: [],
