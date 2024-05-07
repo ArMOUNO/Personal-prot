4c98b96c37651eb6ebe44225260883f7b9019c74
ArMouno <armouno8@gmail.com>
2024-05-07 14:46:27 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      hero:["Sedan SC","serif"],
+    }
+  },
+  plugins: [],
+}
